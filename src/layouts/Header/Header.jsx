a7526035474b485ae6dd3e7fbb1032b9b8bd2fdb
Serj Lyakhov <serjml@list.ru>
2025-10-06 21:08:40 +0300

    feat: add socials

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,8 +1,41 @@
 import './Header.scss'
-export default (props) =>  {
+import Logo from '@/components/Logo'
+import clsx from 'clsx'
+import Socials from '@/components/Socials'
+
+export default (props) => {
+  const { url } = props
+
+  const menuItems = [
+    {label: 'Home', href: '/'},
+    {label: 'Recipes', href: '/recipes'},
+    {label: 'Blog', href: '/blog'},
+    {label: 'Contact', href: '/contact'},
+    {label: 'About us', href: '/about'},
+  ]
+
   return (
-    <header>
-      HEADER
+    <header className="header">
+      <div className="header__iiner container">
+        <Logo className="header__logo" />
+        <nav className="header__menu">
+          <ul className="header__menu-list">
+            {menuItems.map(({label, href}) => (
+              <li className="header__menu-item">
+                <a
+                  class={clsx('header__menu-link',
+                    href === url && 'is-active'
+                  )}
+                  href={href}
+                >
+                  {label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <Socials className="header__soc1als"/>
+      </div>
     </header>
   )
 }
